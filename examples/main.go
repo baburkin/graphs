a039f2b9f934f99d2d10f15acad704091ab18c93
Dmitry Baburkin <baburkin@gmail.com>
2020-03-02 12:51:44 +0300

    Update sample code

--- examples/main.go
+++ examples/main.go
@@ -3,17 +3,23 @@ package examples
 // This is a sample usage
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/baburkin/graphs"
 )
 
 func main() {
 	// Create a graph var
-	graph := graphs.DirectedGraph()
+	// graph := graphs.NewDirectedGraph()
+	graph := graphs.NewDirectedGraph()
 
 	// Create a new reader from a file
-	in := bufio.NewReader(os.File("sample_graph.txt"))
+	in, err := os.Open("sample_graph.txt")
+	if err != nil {
+		fmt.Println("Error opening input file.")
+		os.Exit(2)
+	}
 
 	// Initialize graph from a Reader
 	graph.Init(in)
