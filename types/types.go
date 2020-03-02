c00b59ed445c8de227e6c5d1839098f51770ef69
Dmitry Baburkin <baburkin@gmail.com>
2020-03-02 11:48:51 +0300

    Update types for vertex, edge, directed edge

--- types/types.go
+++ types/types.go
@@ -6,26 +6,34 @@ type Bag interface {
 }
 
 // Vertex type is a generic type for graph's vertices
-type Vertex struct {
-}
+type Vertex interface{}
 
 // Edge type is a generic type for graph's edges
-type Edge struct {
-	source *Vertex
-	dest   *Vertex
+type Edge interface {
+	V() Vertex
+	W() Vertex
 }
 
 // DirectedEdge provides a special kind of edge which has direction.
 // Used in DirectedGraph's
 type DirectedEdge interface {
+	V() Vertex
+	W() Vertex
 }
 
 // UndirectedGraph interface provides API to work with undirected graphs
 type UndirectedGraph interface {
+	AddEdge(v int, w int)
+	Edges() []Edge
+	Vertices() []Vertex
 }
 
 // DirectedGraph interface provides API to work with directed graphs
 type DirectedGraph interface {
+	AddEdge(v int, w int)
 	Edges() []DirectedEdge
 	Vertices() []Vertex
+	OutDegree(vertex int) int
+	InDegree(vertex int) int
+	Reverse() *DirectedGraph
 }
