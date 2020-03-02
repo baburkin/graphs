bc06111f06f9a6693fb51c8fdc37641b41cf41d3
Dmitry Baburkin <baburkin@gmail.com>
2020-03-02 12:51:14 +0300

    Adding DFS, BFS and directed graph implementations

---
+++ graphs/directed_cycle.go
@@ -0,0 +1,10 @@
+package graphs
+
+// Determines if the directed graph is a DAG (directed acyclic graph)
+func (g *Topological) IsDAG() bool {
+
+}
+
+func (g *Topological) Rank(v Vertex) {
+
+}
---
+++ graphs/directed_graph.go
@@ -0,0 +1,8 @@
+package graphs
+
+// NewDirectedGraph creates a new (empty) instance of DirectedGraph
+func NewDirectedGraph() *DirectedGraph {
+	var G *DirectedGraph
+	G = DirectedGraph{}
+	return G
+}
