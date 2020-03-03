6fbb66f7d53c389b77d1ea7cf1c0c561bbb511ef
Dmitry Baburkin <baburkin@gmail.com>
2020-03-03 17:32:56 +0300

    Fix a bug in directed cycle finder

--- directed_cycle.go
+++ directed_cycle.go
@@ -11,7 +11,8 @@ type directedCycle struct {
 }
 
 func (dc *directedCycle) String() string {
-	return fmt.Sprintf("marked: [%v], cycle: [%v]\n", dc.marked, dc.cycle)
+	return fmt.Sprintf("Graph: [%v] marked: [%v], onStack: [%v] cycle: [%v]",
+		dc.DirectedGraph, dc.marked, dc.onStack, dc.cycle)
 }
 
 func initDFSCycle(g DirectedGraph) *directedCycle {
@@ -28,7 +29,6 @@ func findCycleDFS(dc *directedCycle, v int) {
 	dc.onStack[v] = true
 	dc.marked[v] = true
 	for _, w := range dc.Edges(v) {
-
 		// if there is a cycle - return immediately
 		if len(dc.cycle) > 0 {
 			return
@@ -43,8 +43,8 @@ func findCycleDFS(dc *directedCycle, v int) {
 			dc.cycle = append(dc.cycle, w)
 			dc.cycle = append(dc.cycle, v)
 		}
-
 	}
+	dc.onStack[v] = false
 }
 
 // IsDAG determines if the directed graph is a DAG (directed acyclic graph)
