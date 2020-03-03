2b9ee8b546c782a9cd81d9e3692d0d40dbb7ec20
Dmitry Baburkin <baburkin@gmail.com>
2020-03-03 17:34:08 +0300

    Fix topological sort function

--- topological.go
+++ topological.go
@@ -6,13 +6,6 @@ type topoOrder struct {
 	DirectedGraph
 }
 
-// DepthFirstOrder dfs = new DepthFirstOrder(G);
-// order = dfs.reversePost();
-// rank = new int[G.V()];
-// int i = 0;
-// for (int v : order)
-// 	rank[v] = i++;
-
 // Rank shows the rank of the vertex if topological sort exists, -1 otherwise
 func Rank(g DirectedGraph, v Vertex) int {
 	return -1
@@ -21,9 +14,7 @@ func Rank(g DirectedGraph, v Vertex) int {
 // TopoSort sorts the directed graph in topological order and returns the ordered slice of vertices
 func TopoSort(g DirectedGraph) ([]int, error) {
 	if IsDAG(g) {
-		order := make([]int, g.VNum(), g.VNum())
-
-		return order, nil
+		return ReversePostOrder(g), nil
 	}
 	return nil, fmt.Errorf("The graph [%v] has a cycle", g)
 }
