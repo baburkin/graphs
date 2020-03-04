86d6ec2f3fd5c3626ddc50098a9d87eabae85b88
Dmitry Baburkin <baburkin@gmail.com>
2020-03-04 11:11:43 +0300

    Add tests for directed graphs

--- directed_graph_test.go
+++ directed_graph_test.go
@@ -1,2 +1,18 @@
 package graphs
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitGraphByVNum(t *testing.T) {
+	graph := InitDirectedGraph(1000)
+
+	assert.Equal(t, graph.VNum(), 1000)
+	assert.Equal(t, graph.ENum(), 0)
+}
+
+func TestAddEdges(t *testing.T) {
+
+}
