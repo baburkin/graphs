4a688729d4ec36ef43f3873c912a6b7a24e62191
Dmitry Baburkin <baburkin@gmail.com>
2020-03-04 11:01:36 +0300

    Add undirected graph declaration

--- undirected_graph.go
+++ undirected_graph.go
@@ -1,1 +1,11 @@
 package graphs
+
+// Graph interface provides API to work with undirected graphs
+// The edges are unweighted
+type Graph interface {
+	VNum() int                 // Number of vertices
+	ENum() int                 // Number of edges
+	AddEdge(v int, w int) bool // Add edge v -- w
+	Edges(v int) []int         // All adjacent edges to v
+	AllEdges() map[int][]int   // All edges in the graph
+}
