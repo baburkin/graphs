a05729518a6cfaed80147ac14cd936e45437c436
Dmitry Baburkin <baburkin@gmail.com>
2020-03-06 18:47:05 +0300

    Add implementation for connected components search on a graph

---
+++ connected_components.go
@@ -0,0 +1,40 @@
+package graphs
+
+type ccGraph struct {
+	graph   Graph
+	ccIndex []int
+	ccSize  []int
+	marked  []bool
+	count   int
+}
+
+// ConnectedComponents searches the given graph for connected components
+// and returns arrays c []int and s []int, where the value c[v] is an id
+// of a component, to which the given vertex 'v' belongs, and s[i] is the size
+// of component with id 'i'.
+func ConnectedComponents(g Graph) ([]int, []int) {
+	cc := new(ccGraph)
+	cc.graph = g
+	cc.ccIndex = make([]int, 0, g.VNum())
+	cc.ccSize = make([]int, 0, g.VNum()/2)
+	cc.marked = make([]bool, 0, g.VNum())
+	cc.count = 0
+	for v := 0; v < g.VNum(); v++ {
+		if !cc.marked[v] {
+			runDFS(cc, v)
+			cc.count++
+		}
+	}
+	return cc.ccIndex, cc.ccSize
+}
+
+func runDFS(cc *ccGraph, v int) {
+	cc.marked[v] = true
+	cc.ccIndex[v] = cc.count
+	cc.ccSize[cc.count]++
+	for _, w := range cc.graph.Edges(v) {
+		if !cc.marked[w] {
+			runDFS(cc, w)
+		}
+	}
+}
