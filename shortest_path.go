657815222506f1235c75fd473b885b9673d4cc13
Dmitry Baburkin <baburkin@gmail.com>
2020-09-27 20:41:45 +0300

    Add shortest path calculation algorithm using BFS (breadth-first search)

---
+++ shortest_path.go
@@ -0,0 +1,58 @@
+package graphs
+
+import (
+	"math"
+)
+
+// ShortestPathBFS returns the slice of vertices that form
+// the shortest path between `vSource` and `vTarget` vertices:
+//
+// vSource -> v1 -> v2 -> ... -> vTarget
+//
+// The slice returns includes vSource but does not include
+// vTarget, thus length of the slice equals to the number of hops.
+//
+// If vSource == vTarget then zero-length slice is returned
+func ShortestPathBFS(g Graph, vSource int, vTarget int) []int {
+	var path []int
+	var queue []int
+	var marked []bool
+	var distTo []int
+	var edgeTo []int
+
+	marked = make([]bool, g.VNum(), g.VNum())
+	distTo = make([]int, g.VNum(), g.VNum())
+	edgeTo = make([]int, g.VNum(), g.VNum())
+	queue = make([]int, 0, g.VNum()-1)
+
+	edgeTo[vSource] = vSource
+	distTo[vSource] = 0
+	marked[vSource] = true
+
+	// Init distTo with "Infinity"
+	for v := 0; v < g.VNum(); v++ {
+		if v != vSource {
+			distTo[v] = int(math.MaxInt64)
+		}
+	}
+
+	// BFS algorithm to search for shortest path
+	queue = append(queue, vSource)
+	for len(queue) != 0 {
+		v := queue[0]
+		queue = queue[1:]
+		for _, w := range g.Edges(v) {
+			edgeTo[w] = v
+			distTo[w] = distTo[v] + 1
+			marked[w] = true
+			queue = append(queue, w)
+		}
+	}
+
+	// Path includes succession of vertices: [vSouce, v1, ..., vTarget]
+	for v := edgeTo[vTarget]; distTo[v] != 0; v = edgeTo[v] {
+		path = append([]int{v}, path...)
+	}
+
+	return path
+}
