1b2e88b7d85b8110b77dd2479c7fc0362c6c3d3b
Dmitry Baburkin <baburkin@gmail.com>
2020-09-29 15:09:41 +0300

    Rework tests for directed graphs

--- directed_graph_test.go
+++ directed_graph_test.go
@@ -9,17 +9,30 @@ import (
 
 func TestInitDirectedGraphByVNum(t *testing.T) {
 	graph := InitDirectedGraph(1000)
-
-	assert.Equal(t, graph.VNum(), 1000)
-	assert.Equal(t, graph.ENum(), 0)
+	testData := []struct {
+		parameter string
+		actual    int
+		expected  int
+	}{
+		{"VNum()", graph.VNum(), 1000},
+		{"ENum()", graph.ENum(), 0},
+	}
+	for _, test := range testData {
+		t.Logf("Parameter: [%v], expected: [%v], actual: [%v]", test.parameter, test.expected, test.actual)
+		if test.actual != test.expected {
+			t.Fail()
+		}
+	}
 }
 
 func TestStringReprOfDirectedGraph(t *testing.T) {
 	graph := InitDirectedGraph(3)
 	graph.AddEdge(1, 2)
-	assert.Equal(t,
-		"DirectedGraph(vertices: [3]; edges: [[] [2] []]; in degree [0 0 1])",
-		fmt.Sprintf("%v", graph))
+	actual := fmt.Sprintf("%v", graph)
+	expected := "DirectedGraph(vertices: [3]; edges: [[] [2] []]; in degree [0 0 1])"
+	if actual != expected {
+		t.Errorf("Expected: [%v], actual: [%v]", expected, actual)
+	}
 }
 
 func TestAddEdges(t *testing.T) {
@@ -28,8 +41,11 @@ func TestAddEdges(t *testing.T) {
 	graph.AddEdge(4, 1)
 	graph.AddEdge(4, 2)
 	graph.AddEdge(4, 3)
-
-	assert.Equal(t, graph.ENum(), 4)
+	actual := graph.ENum()
+	expected := 4
+	if actual != expected {
+		t.Errorf("Expected: [%v], actual: [%v]", expected, actual)
+	}
 }
 
 func TestInitDirectedGraphFromFile(t *testing.T) {
@@ -40,7 +56,7 @@ func TestInitDirectedGraphFromFile(t *testing.T) {
 	assert.Equal(t, graph.ENum(), 7)
 }
 
-func TestInitDirectedGraphFromFile2(t *testing.T) {
+func TestInitDirectedGraphFromFileErr(t *testing.T) {
 	graph, err := LoadDirectedGraph("examples/directed_err.txt")
 
 	if assert.Error(t, err) {
